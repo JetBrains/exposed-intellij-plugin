d92ce805656072788580ef09ffb2692f79901c80
Darya Sharkova <darya.sharkova@jetbrains.com>
2020-07-02 16:49:19 +0300

    Added the gradle plugin module template.

---
+++ exposed-gradle/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.72"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
