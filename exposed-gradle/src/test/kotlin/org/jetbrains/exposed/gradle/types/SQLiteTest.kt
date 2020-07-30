071ff91c017f10b03eef3d2ddc722ed0396b8441
Darya Sharkova <darya.sharkova@jetbrains.com>
2020-07-30 22:09:52 +0300

    Rewrote SQLite test to use reflection instead of line-by-line comparison.

--- exposed-gradle/src/test/kotlin/org/jetbrains/exposed/gradle/types/SQLiteTest.kt
+++ exposed-gradle/src/test/kotlin/org/jetbrains/exposed/gradle/types/SQLiteTest.kt
@@ -1,36 +1,80 @@
 package org.jetbrains.exposed.gradle.types
 
+import com.tschuchort.compiletesting.KotlinCompilation
 import org.jetbrains.exposed.gradle.DatabaseFromScriptTest
+import org.jetbrains.exposed.gradle.ExposedCodeGeneratorFromScriptTest
 import org.jetbrains.exposed.gradle.resourcesTestDataPath
 import org.jetbrains.exposed.gradle.tests.TestDB
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.`java-time`.JavaLocalDateColumnType
+import org.jetbrains.exposed.sql.`java-time`.JavaLocalDateTimeColumnType
 import org.junit.Test
 import java.nio.file.Paths
 
-class SQLiteTest : DatabaseFromScriptTest() {
-    private fun runSQLiteTest(filename: String, tableName: String? = null) {
-        testFromScriptAgainstKtFile(
-                Paths.get(resourcesTestDataPath.toString(), "sqlite", "vartypes.sql"),
-                Paths.get("sqlite", filename),
-                tableName = tableName,
-                excludedDbList = TestDB.enabledInTests() - listOf(TestDB.SQLITE)
+class SQLiteTestC : ExposedCodeGeneratorFromScriptTest() {
+    private fun runSQLiteTest(tableName: String, checkTablesBlock: (KotlinCompilation.Result) -> Unit) {
+        testByCompilation(
+                "vartypes.sql",
+                Paths.get(resourcesTestDataPath.toString(), "sqlite"),
+                checkTablesBlock,
+                TestDB.enabledInTests() - listOf(TestDB.SQLITE),
+                tableName
         )
     }
 
     @Test
-    fun integerTypes() = runSQLiteTest("IntegerTypes.kt", "integer_types")
+    fun integerTypes() = runSQLiteTest("integer_types") { result ->
+        checkTableObject(result, "IntegerTypes", "integer_types", { tableObject ->
+            checkColumnProperty(result, tableObject, "i1", "i1", IntegerColumnType())
+            checkColumnProperty(result, tableObject, "i2", "i2", IntegerColumnType())
+            checkColumnProperty(result, tableObject, "i3", "i3", IntegerColumnType())
+            checkColumnProperty(result, tableObject, "i4", "i4", ByteColumnType())
+            checkColumnProperty(result, tableObject, "i5", "i5", ShortColumnType())
+            checkColumnProperty(result, tableObject, "i6", "i6", ShortColumnType())
+        })
+    }
 
     @Test
-    fun floatingPointTypes() = runSQLiteTest("FloatingPointTypes.kt", "floating_point_types")
+    fun floatingPointTypes() = runSQLiteTest("floating_point_types") { result ->
+        checkTableObject(result, "FloatingPointTypes", "floating_point_types", { tableObject ->
+            checkColumnProperty(result, tableObject, "f1", "f1", FloatColumnType())
+            checkColumnProperty(result, tableObject, "f2", "f2", DoubleColumnType())
+            checkColumnProperty(result, tableObject, "f3", "f3", DoubleColumnType())
+            checkColumnProperty(result, tableObject, "f4", "f4", DoubleColumnType())
+        })
+    }
 
     @Test
-    fun decimalTypes() = runSQLiteTest("DecimalTypes.kt", "decimal_types")
+    fun longTypes() = runSQLiteTest("long_types") { result ->
+        checkTableObject(result, "LongTypes", "long_types", { tableObject ->
+            checkColumnProperty(result, tableObject, "l1", "l1", LongColumnType())
+            checkColumnProperty(result, tableObject, "l2", "l2", LongColumnType())
+        })
+    }
 
     @Test
-    fun longTypes() = runSQLiteTest("LongTypes.kt", "long_types")
+    fun decimalTypes() = runSQLiteTest("decimal_types") { result ->
+        checkTableObject(result, "DecimalTypes", "decimal_types", { tableObject ->
+            checkColumnProperty(result, tableObject, "d1", "d1", DecimalColumnType(131072, 10))
+            checkColumnProperty(result, tableObject, "d2", "d2", DecimalColumnType(10, 5))
+        })
+    }
 
     @Test
-    fun charTypes() = runSQLiteTest("CharTypes.kt", "char_types")
+    fun charTypes() = runSQLiteTest("char_types") { result ->
+        checkTableObject(result, "CharTypes", "char_types", { tableObject ->
+            checkColumnProperty(result, tableObject, "c1", "c1", CharColumnType(19))
+            checkColumnProperty(result, tableObject, "c2", "c2", VarCharColumnType(255))
+            checkColumnProperty(result, tableObject, "c3", "c3", VarCharColumnType(255))
+            checkColumnProperty(result, tableObject, "c4", "c4", TextColumnType())
+        })
+    }
 
     @Test
-    fun datetimeTypes() = runSQLiteTest("DatetimeTypes.kt", "datetime_types")
+    fun datetimeTypes() = runSQLiteTest("datetime_types") { result ->
+        checkTableObject(result, "DatetimeTypes", "datetime_types", { tableObject ->
+            checkColumnProperty(result, tableObject, "d1", "d1", JavaLocalDateTimeColumnType())
+            checkColumnProperty(result, tableObject, "d2", "d2", JavaLocalDateColumnType())
+        })
+    }
 }
--- exposed-gradle/src/test/resources/databases/sqlite/vartypes.sql
+++ exposed-gradle/src/test/resources/databases/sqlite/vartypes.sql
@@ -12,10 +12,10 @@ l2 int8 NOT NULL
 );
 INSERT INTO long_types VALUES(462374682332,545794879594);
 CREATE TABLE char_types(
-ch1 character(19) NOT NULL,
-ch2 varchar(255) NOT NULL,
-ch3 varying character(255) NOT NULL,
-ch4 text NOT NULL
+c1 character(19) NOT NULL,
+c2 varchar(255) NOT NULL,
+c3 varying character(255) NOT NULL,
+c4 text NOT NULL
 );
 INSERT INTO char_types VALUES('nineteen symbols!!!','a string of arbitrary length no more than 255','another string of arbitrary length','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet convallis pretium. Nulla dictum sapien id massa viverra mollis. Curabitur congue bibendum eleifend. Proin placerat libero arcu, id pellentesque urna sagittis eu. Cras semper ipsum at tortor accumsan pretium. Sed nibh purus, volutpat ac egestas in, dictum at felis. Integer sodales tellus ipsum, quis egestas massa dapibus fermentum. Aenean ultrices sed lectus in viverra. In eget urna pellentesque, rutrum magna non, semper dui. Vivamus vel consectetur velit. Ut porttitor nibh sed tellus viverra, sed euismod ante vulputate. Etiam in mauris lacinia, maximus quam at, vulputate massa. Curabitur et convallis ligula, interdum pretium felis.');
 CREATE TABLE floating_point_types(
@@ -31,8 +31,8 @@ b2 blob NOT NULL
 );
 INSERT INTO miscellanious_types VALUES(0,'abracadabra?');
 CREATE TABLE decimal_types(
-n1 numeric NOT NULL,
-n2 decimal(10, 5) NOT NULL
+d1 numeric NOT NULL,
+d2 decimal(10, 5) NOT NULL
 );
 INSERT INTO decimal_types VALUES(46234673,1234567890.1234500408);
 CREATE TABLE integer_types(
