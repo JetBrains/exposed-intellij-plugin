530214866fba147ff25f4328388fed31f4b29eac
Andrey.Tarashevskiy <andrey.tarashevskiy@jetbrains.com>
2020-09-12 15:47:42 +0300

    Update plugin links and version

--- exposed-gradle-plugin/plugin-build/buildSrc/src/main/java/Coordinates.kt
+++ exposed-gradle-plugin/plugin-build/buildSrc/src/main/java/Coordinates.kt
@@ -1,20 +1,20 @@
 object PluginCoordinates {
     const val ID = "com.jetbrains.exposed.gradle.plugin"
     const val GROUP = "com.jetbrains.exposed.gradle"
-    const val VERSION = "1.0.0"
+    const val VERSION = "0.0.1"
     const val IMPLEMENTATION_CLASS = "com.jetbrains.exposed.gradle.plugin.ExposedGradlePlugin"
 }
 
 object PluginBundle {
-    const val VCS = "https://github.com/cortinico/kotlin-gradle-plugin-template"
-    const val WEBSITE = "https://github.com/cortinico/kotlin-gradle-plugin-template"
-    const val DESCRIPTION = "An empty Gradle plugin created from a template"
-    const val DISPLAY_NAME = "An empty Gradle Plugin from a template"
+    const val VCS = "https://github.com/JetBrains/exposed-intellij-plugin"
+    const val WEBSITE = "https://github.com/JetBrains/exposed-intellij-plugin"
+    const val DESCRIPTION = "Exposed ORM framework plugin"
+    const val DISPLAY_NAME = "Exposed ORM framework plugin"
     val TAGS = listOf(
         "plugin",
-        "gradle",
-        "sample",
-        "template"
+        "kotlin",
+        "exposed",
+        "database"
     )
 }
 
